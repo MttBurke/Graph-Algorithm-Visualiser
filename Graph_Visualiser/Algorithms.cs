ec88b5af3507254a2ea812a71eb9f1e4db221596
MttBurke <mb.525@outlook.com>
2022-05-15 17:24:05 +0100

    Started project added base to program.

---
+++ Graph_Visualiser/Algorithms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Visualiser
+{
+    class Algorithms
+    {
+        private string _name;
+        private byte _nodes_no;
+    }
+}
